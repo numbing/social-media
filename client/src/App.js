f662f575aabffa9dbf4bdffdcd9563a9552476ae
mir <mirnumbing@gmail.com>
2018-12-30 12:08:50 +0100

    profile layout

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 import PrivateRoute from "./components/common/PrivateRoute";
 
@@ -58,6 +59,13 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
