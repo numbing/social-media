67256a4bdf2ed985e744b23a6c456d9f3149529e
mir <mirnumbing@gmail.com>
2018-12-30 04:31:31 +0100

    login authinticated

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register User
 
@@ -12,4 +16,42 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//Login  - Get user Token
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/user/login", userData)
+    .then(res => {
+      //save to localstorage
+      const { token } = res.data;
+
+      //set token to localstorage
+
+      localStorage.setItem("jwtToken", token);
+
+      //Set token to Auth header
+
+      setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      //set Current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//set logged in user
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
