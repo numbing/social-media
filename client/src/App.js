5375740197764dcb01ffa5c8fd657e093ef2a383
mir <mirnumbing@gmail.com>
2019-01-01 10:14:11 +0100

    add experience interface

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 
+import AddExperience from "./components/add-credentials/AddExperience";
+
 import PrivateRoute from "./components/common/PrivateRoute";
 
 import "./App.css";
@@ -74,6 +76,13 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
